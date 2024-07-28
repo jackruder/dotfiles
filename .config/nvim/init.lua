6f4f0bf8711ac2360cac22ad2b34bca17c19c437
Jack Ruder <jackruder14@gmail.com>
2024-07-27 17:50:07 -0700

    Fix .gitignore

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,11 +15,6 @@ package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
 local luarocks_cpath = {
 vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
 vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
--- Remove the dylib and dll paths if you do not need macos or windows support
-vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dylib"),
-vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dylib"),
-vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.dll"),
-vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.dll"),
 }
 package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
 
